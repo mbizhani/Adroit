3eee8cd533b05e05dc0c19dd3d60c6df761ad4bc
Mehdi Bizhani <mbizhani@gmail.com>
2018-03-08 20:37:29 +0330

    for convenience, the RowVO is annotated with XStream MapConverter

--- src/main/java/org/devocative/adroit/sql/result/RowVO.java
+++ src/main/java/org/devocative/adroit/sql/result/RowVO.java
@@ -1,7 +1,11 @@
 package org.devocative.adroit.sql.result;
 
+import com.thoughtworks.xstream.annotations.XStreamConverter;
+import com.thoughtworks.xstream.converters.collections.MapConverter;
+
 import java.util.LinkedHashMap;
 
+@XStreamConverter(MapConverter.class)
 public class RowVO extends LinkedHashMap<String, Object> {
 	private static final long serialVersionUID = 8800701053699750620L;
 }
